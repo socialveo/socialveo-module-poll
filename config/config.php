1f450fbc0487e800a2e9c0aebd326a9fd752b5cb
macagoraga <info@macagoraga.com>
2015-08-27 14:08:33 +0200

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Socialveo Poll custom module config
+ *
+ * @author      Socialveo.com Dev Team
+ * @license     MIT License
+ * @package     Poll
+ * @since       1.0
+ * @version     1.0
+ */
+
+return new \Phalcon\Config([]);
